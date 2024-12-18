eed91f545181c3e1ea9067b777a621d68051bf6d
DevJaimeGR <dev.backend.jigr@hotmail.com>
2024-12-18 16:18:35 -0500

    add Perfect pay project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUI.Project.PerfectPay
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI.Project.PerfectPay
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
